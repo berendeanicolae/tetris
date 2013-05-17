f0b6e80b77868afebe23cc57446bc6481eb86394
berendeanicolae <berendeanicolae@gmail.com>
2013-05-17 20:47:35 +0000

    Update game.h

--- game.h
+++ game.h
@@ -1,5 +1,6 @@
 #include "tetromino.h"
 #include <SDL/SDL_mixer.h>
+#include <SDL/SDL_ttf.h>
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
 
@@ -7,7 +8,9 @@ class Game{
     //Variables
     private:
         const SDL_VideoInfo *vinfo;
+        Mix_Chunk *pop;
         SDL_Surface *screen,*background;
+        TTF_Font *font;
         bool quit;
         Sint16 boxh,board_x,board_y;
         int level,to_next_lv,points;
@@ -24,11 +27,11 @@ class Game{
         void draw_play();
         void menu();
         void play();
-        void Tetromino_move(Tetromino *fig[3], bool **grid, key keyboard[4], tetromino_list **tetrominos, int l);
+        void update_score();
+        bool Tetromino_move(Tetromino *fig[3], bool **grid, key keyboard[4], tetromino_list **tetrominos, int l); //returns true if tetromino can not enter
         void Tetromino_delete(Tetromino *fig);
-        void Tetromino_down_end(Tetromino *fig[3], bool **grid, tetromino_list **tetrominos, int l);
+        bool Tetromino_down_end(Tetromino *fig[3], bool **grid, tetromino_list **tetrominos, int l);
         void replace_with(SDL_Surface *from, short x, short y);
-        Mix_Chunk *pop;
 };
 
 #endif // GAME_H_INCLUDED
